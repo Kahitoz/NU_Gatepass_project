a23dfc187a89f23b7e8ea0ea8717218239725969
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-23 00:00:36 +0530

    Added Routes for student

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Student from './StudentComponent/Student';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Router>
+        <Routes>
+        <Route exact path = "*" element={<Student/>}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
