8393fbbe9616d1f65d735c06bd797c928aa50c27
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-23 00:00:54 +0530

    Done Some Implementation in student page

---
+++ client_side/src/StudentComponent/student.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Student = () =>{
+return(
+    <div>
+        <div>
+            <h1>Student Page</h1>
+        </div>
+    </div>
+)
+}
+export default Student;
