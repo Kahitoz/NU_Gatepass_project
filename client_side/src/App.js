e3179914673292ae3183f4c8177921e178e7b048
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-06-23 03:05:31 +0530

    Code Formatted

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,17 +1,16 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Student from './StudentComponent/StudentsDashboard/Student';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Student from "./StudentComponent/StudentsDashboard/Student";
 
 function App() {
-  return(
+  return (
     <div>
       <Router>
         <Routes>
-        <Route exact path = "*" element={<Student/>}></Route>
+          <Route exact path="*" element={<Student />}></Route>
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
 export default App;
--- client_side/src/StudentComponent/StudentsDashboard/Student.js
+++ client_side/src/StudentComponent/StudentsDashboard/Student.js
@@ -2,28 +2,38 @@ import React from "react";
 import Niit_logo from "../../StudentComponent/icons/icon-niit.png";
 import Logout from "../../StudentComponent/icons/icon-logout.png";
 import Strings from "../StudentsDashboard/Strings.json";
-import designs from "../StudentsDashboard/CSShandler.json"
+import designs from "../StudentsDashboard/CSShandler.json";
 
 const Student = () => {
-    return (
-        <div >
-            <div>
-                <nav className={`${designs.Navbar["d-1"]}`}>
-                    <div className={`${designs.Navbar["d-2"]}`}>
-                        <img src={Niit_logo} alt={`${Strings.navbar_heading_1}`} className={`${designs.Navbar["d-3"]}`} />
-                        <h1 className={`${designs.Navbar["d-4"]}`}>{Strings.navbar_heading_1}</h1>
-                    </div>
-                    <div className={`${designs.Navbar["d-5"]}`}>
-                        <ul className={`${designs.Navbar["d-6"]}`}>
-                            <li className={`${designs.Navbar["d-7"]}`}>{Strings.list_1}</li>
-                            <li className={`${designs.Navbar["d-7"]}`}>{Strings.list_2}</li>
-                        </ul>
-                        <img src={Logout} alt={`${Strings["alt-1"]}`} className={`${designs.Navbar["d-8"]}`} />
-                    </div>
-                </nav>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <nav className={`${designs.Navbar["d-1"]}`}>
+          <div className={`${designs.Navbar["d-2"]}`}>
+            <img
+              src={Niit_logo}
+              alt={`${Strings.navbar_heading_1}`}
+              className={`${designs.Navbar["d-3"]}`}
+            />
+            <h1 className={`${designs.Navbar["d-4"]}`}>
+              {Strings.navbar_heading_1}
+            </h1>
+          </div>
+          <div className={`${designs.Navbar["d-5"]}`}>
+            <ul className={`${designs.Navbar["d-6"]}`}>
+              <li className={`${designs.Navbar["d-7"]}`}>{Strings.list_1}</li>
+              <li className={`${designs.Navbar["d-7"]}`}>{Strings.list_2}</li>
+            </ul>
+            <img
+              src={Logout}
+              alt={`${Strings["alt-1"]}`}
+              className={`${designs.Navbar["d-8"]}`}
+            />
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Student;
