5f4da2035ae018b5f44fb2557f44870b3a82f461
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-26 19:24:18 +0530

    Made the values for the widgit

--- client_side/src/StudentComponent/StudentSkeleton/S3_Widgets.js
+++ client_side/src/StudentComponent/StudentSkeleton/S3_Widgets.js
@@ -3,15 +3,23 @@ const S3_Widgets = () => {
   return (
     <div className={`${designs.d1}`}>
       <div className={`${designs.d2 }`}>
-        <h1>Local Fixed Gatepass Available: 3</h1>
+      <div className={`items-center justify-center text-center flex flex-col sm:flex-row`}>
+            <h1 className={`font-bold me-2`}>Local Fixed Gatepass Available:</h1>
+            <p>3</p>
+      </div>
+    
       </div>
 
       <div className={`${designs.d2}`}>
-        <h1>Gatepass Allowed</h1>
+        <h1 className={`text-green-500 font-bold`}>Gatepass Allowed</h1>
       </div>
 
       <div className={`${designs.d2}`}>
-        <h1>Last Gatepass Status</h1>
+      <div className={`items-center justify-center text-center flex flex-col sm:flex-row`}>
+         <h1 className={`font-bold me-2`}>Last Gatepass Status:</h1>
+         <p className={`text-blue-700`}>Pending</p>
+      </div>
+       
       </div>
     </div>
   );
