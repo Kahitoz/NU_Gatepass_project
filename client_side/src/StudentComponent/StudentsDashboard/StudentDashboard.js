8347b3cfbec92e1855bb23f844fb77625a9f2fe6
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-06-27 11:07:55 +0530

    Added Background

--- client_side/src/StudentComponent/StudentsDashboard/StudentDashboard.js
+++ client_side/src/StudentComponent/StudentsDashboard/StudentDashboard.js
@@ -1,19 +1,23 @@
 import React from "react";
 import StudentNavbar from "../StudentSkeleton/S1_Navbar";
 import StudentSubNavbar from "../StudentSkeleton/S2_SubNavbar";
-import Widgets from "../StudentSkeleton/S3_Widgets"
+import Widgets from "../StudentSkeleton/S3_Widgets";
+import Table from "../StudentSkeleton/S4_Table";
 
 const StudentDashboard = () => {
   return (
-    <div>
+    <div className="bg-background w-screen h-screen">
       <div>
         <StudentNavbar />
       </div>
       <div>
-        <StudentSubNavbar/>
+        <StudentSubNavbar />
       </div>
       <div>
-      <Widgets/>
+        <Widgets />
+      </div>
+      <div>
+        <Table />
       </div>
     </div>
   );
