0c858d44cee2c0b48238aaec971dc12fb5294425
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-28 15:34:19 +0530

    added login routes

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StudentHome from "./StudentComponent/StudentsScreen/S1_Dashboard";
 import StudentGatepass from "./StudentComponent/StudentsScreen/S2_Gatepasses"
+import Login from "./OAuthComponent/O1_Login"
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
+          <Route exact path = "/login" element={<Login/>}></Route>
           <Route exact path="/student/home" element={<StudentHome />}></Route>
           <Route exact path="/student/gatepass" element={<StudentGatepass/>}></Route>
         </Routes>
