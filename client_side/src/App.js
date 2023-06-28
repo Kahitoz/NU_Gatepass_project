e94ec1d64c4b9fc99f8ab5c5128541cc833e9696
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-06-28 12:50:28 +0530

    Added route for student gatepass

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Student from "./StudentComponent/StudentsScreen/S1_Dashboard";
+import StudentHome from "./StudentComponent/StudentsScreen/S1_Dashboard";
+import StudentGatepass from "./StudentComponent/StudentsScreen/S2_Gatepasses"
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route exact path="/student/home" element={<Student />}></Route>
+          <Route exact path="/student/home" element={<StudentHome />}></Route>
+          <Route exact path="/student/gatepass" element={<StudentGatepass/>}></Route>
         </Routes>
       </Router>
     </div>
