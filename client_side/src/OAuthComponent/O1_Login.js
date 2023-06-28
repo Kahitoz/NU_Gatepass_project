751d17918d4cc9d899bf0ccb664e87c1ec927640
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-28 15:34:35 +0530

    made the login

---
+++ client_side/src/OAuthComponent/O1_Login.js
@@ -0,0 +1,9 @@
+import LoginForm from "./O1_skeleton"
+const O1_Login = () =>{
+    return(
+        <div className="bg-background w-sceen h-screen">
+        <LoginForm/>
+        </div>
+    )
+}
+export default O1_Login
