991d5b9df0fcf6ce2f608f79cb4a4d1ac5141194
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-29 20:06:35 +0530

    Completed the backend

--- client_side/src/StudentComponent/StudentSkeleton/S5_Profile.js
+++ client_side/src/StudentComponent/StudentSkeleton/S5_Profile.js
@@ -1,13 +1,45 @@
 import profile from "../icons/icon-profile.png";
-import designs from "../StudentStyling/S5_ProfileCSS"
+import designs from "../StudentStyling/S5_ProfileCSS";
+import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
+import React, { useEffect, useState } from "react";
+
 const S5_Profile = () => {
+  const [userName, setUserName] = useState("");
+  const [studentName, setStudentName] = useState("");
+  const [studentHostel, setStudentHostel] = useState("");
+  const [studentEnrollment, setStudentEnrollment] = useState("");
+  const [studentContact, setStudentContact] = useState("");
+
+  useEffect(() => {
+    const userToken = Cookies.get("ACCESS_TOKEN");
+    const decoded = jwt_decode(userToken);
+    const email = decoded.data.email_id;
+
+    async function fetchUserDetails() {
+      const response = await fetch(`http://127.0.0.1:4000/gatepass/v2/user_details/${email}`, {
+        headers: {
+          Authorization: userToken,
+        },
+      });
+      const data = await response.json();
+      setUserName(data.name);
+      setStudentName(data.name);
+      setStudentHostel(data.hostel);
+      setStudentEnrollment(data.user_id);
+      setStudentContact(data.contact_number);
+    }
+
+    fetchUserDetails();
+  }, []);
+
   return (
     <div className={`${designs.d1}`}>
       <div className={`${designs.d2}`}>
         <div className={`${designs.d3}`}>
           <div className={`${designs.d4}`}>
             <div className={`${designs.d6}`}>
-              <img src={profile} alt="profile"></img>
+              <img src={profile} alt="profile" />
             </div>
             <div className={`${designs.d7}`}>
               <div className={`${designs.d8}`}>
@@ -25,21 +57,29 @@ const S5_Profile = () => {
               type="text"
               className={`${designs.d13}`}
               placeholder="Student Name"
+              value={studentName}
+              disabled
             />
             <input
               type="text"
               className={`${designs.d13}`}
               placeholder="Student Hostel"
+              value={studentHostel}
+              disabled
             />
             <input
               type="text"
               className={`${designs.d13}`}
               placeholder="Student Enrolment"
+              value={studentEnrollment}
+              disabled
             />
             <input
               type="text"
               className={`${designs.d13}`}
               placeholder="Student Contact Number"
+              value={studentContact}
+              disabled
             />
           </div>
         </div>
@@ -47,4 +87,5 @@ const S5_Profile = () => {
     </div>
   );
 };
+
 export default S5_Profile;
