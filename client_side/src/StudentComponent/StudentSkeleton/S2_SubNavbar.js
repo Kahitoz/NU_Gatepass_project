60ef6c53e318e2b45f7c13e3b4256f4c2501d86e
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-29 20:00:41 +0530

    Completed SubNavbar backend

--- client_side/src/StudentComponent/StudentSkeleton/S2_SubNavbar.js
+++ client_side/src/StudentComponent/StudentSkeleton/S2_SubNavbar.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Clouds from "../icons/icon-clouds.png";
 import designs from "../StudentStyling/S2_SubNavbarCSS"
+import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
 const StudentSubNavbar = () => {
+  const [userName, setUserName] = useState("");
+  useEffect(() => {
+    
+    const userToken = Cookies.get("ACCESS_TOKEN");
+    const decoded = jwt_decode(userToken);
+    const email = decoded.data.email_id;
+    
+    async function fetchUserDetails() {
+      const response = await fetch(`http://127.0.0.1:4000/gatepass/v2/user_details/${email}`, {
+        headers: {
+          Authorization: userToken,
+        },
+      });
+      const data = await response.json();
+      setUserName(data.name);
+    }
+
+    fetchUserDetails();
+  }, []);
+
   return (
     <div className={`${designs.subnav.d1}`}>
 
       <div className={`${designs.subnav.d2}`}>
-        <h1 className={`${designs.subnav.d3}`}>Welcome Students</h1>
+        <h1 className={`${designs.subnav.d3}`}>Welcome, {userName}</h1>
         <div className={`${designs.subnav.d4} `}>
           <img src={Clouds} alt="message" className={`${designs.subnav.d5}`}></img>
           <p className={`${designs.subnav.d6}`}>Weather message</p>
