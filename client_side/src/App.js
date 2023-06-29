1cb73914f4bc5e74ca44479174163d5a3ced5c5a
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-06-29 10:57:46 +0530

    made Route

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -5,6 +5,7 @@ import StudentHome from "./StudentComponent/StudentsScreen/S1_Dashboard";
 import StudentGatepass from "./StudentComponent/StudentsScreen/S2_Gatepasses"
 import Login from "./OAuthComponent/O1_Login"
 import WardenApplyLeave from "./WardenComponent/WardenScreens/WardenApplyLeave";
+import WardenReport from "./WardenComponent/WardenScreens/WardenReport";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route exact path="/guard" element={<Guard />}></Route>
           <Route exact path="/Warden/home" element={<Warden />}></Route>
+          <Route exact path="/Warden/reports" element={<WardenReport/>}></Route>
           <Route exact path="/Warden/ApplyLeave" element={<WardenApplyLeave/>}></Route>
           <Route exact path = "/" element={<Login/>}></Route>
           <Route exact path="/student/home" element={<StudentHome />}></Route>
