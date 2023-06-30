bde3059d554afc0a5a1c3e76aec18f009726ce1a
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-06-30 13:21:20 +0530

    Added parameter functions

--- client_side/src/StudentComponent/StudentGatepassHandler/S1_ParameterConfig.js
+++ client_side/src/StudentComponent/StudentGatepassHandler/S1_ParameterConfig.js
@@ -1,46 +1,22 @@
-import React, { useEffect, useState } from "react";
-import S1_LocalFixed from "./S1_LocalFixed";
-import Cookies from "js-cookie";
-import jwt_decode from "jwt-decode"
+var week = async (accessToken) => {
+  let weekLimit = 0;
+  let departureTime = null;
+  let arrivalTime = null;
+  
+  const response = await fetch("http://127.0.0.1:4000/gatepass/v2/admin/parameter_config", {
+    headers: {
+      Authorization: accessToken,
+    },
+  });
+  
+  const text = await response.json();
+  console.log(text);
+  weekLimit = text[0]["value"];
+  departureTime = text[1]["value"];
+  arrivalTime = text[2]["value"];
+  
+  var json_obj = { weekLimit: weekLimit, departureTime: departureTime, arrivalTime: arrivalTime };
+  return json_obj;
+};
 
-const S1_ParameterConfig = () => {
-  const [weekLimit, setWeekLimit] = useState(0);
-  const [departureTime, setDepartureTime] = useState("00:00:00");
-  const [arrivalTime, setarrivalTime] = useState("00:00:00");
-  const accessToken = Cookies.get("ACCESS_TOKEN");
-  const decoded = jwt_decode(accessToken);
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:4000/gatepass/v2/admin/parameter_config", {
-      headers: {
-        Authorization: accessToken,
-      },
-    })
-      .then((response) => response.json())
-      .then((text) => {
-        setWeekLimit(text[0]["value"]);
-        setDepartureTime(text[1]["value"]);
-        setarrivalTime(text[2]["value"]);
-      });
-  }, []);
-
-  return (
-    <div className="admin">
-     
-      <div className="adminContainer">
-      
-        <div className="listContainer">
-          <div className="listTitle">Local Fixed Gatepass</div>
-
-          <S1_LocalFixed
-            departureTime={departureTime}
-            arrivalTime={arrivalTime}
-            weekLimit={weekLimit}
-          />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default S1_ParameterConfig;
+export { week };
