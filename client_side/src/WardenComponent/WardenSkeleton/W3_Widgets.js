48439783134f8bdb7bd1414c724d0686a190183e
Karam <Karam.arora20@st.niituniversity.in>
2023-07-06 01:09:07 +0530

    widget changes

--- client_side/src/WardenComponent/WardenSkeleton/W3_Widgets.js
+++ client_side/src/WardenComponent/WardenSkeleton/W3_Widgets.js
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import designs from "../WardenStyling/W3_WidgetsCSS"
-const W3_Widgets = ({setGpDropdown}) => {
-  const dropdown_values=["MyGatepassRequest","OthersGatepassRequest"];
+import { useState } from "react";
+const W3_Widgets = ({setGpDropdown,subNavOption}) => {
+  const[dropdownValues,setDropdownValues]=useState(["MyGatepassRequest","OthersGatepassRequest"]);
+  useEffect(()=>{
+    if (subNavOption==="Pending Requests"){
+      setDropdownValues(["MyGatepassRequest","OthersGatepassRequest"]);
+    }
+    else if(subNavOption==="Approved / Cancelled"){
+      setDropdownValues(["Approved","Cancelled","All "]);
+      
+    }
+  },[subNavOption])
   return (
     <div className={`${designs.d1}`}>
       <div className={`${designs.d2 }`}>
       <div className={`items-center justify-center text-center flex flex-col sm:flex-row`}>
             <select name="GatepassDropdown" onChange={(e) => setGpDropdown(e.target.value)}>
-                <option value={dropdown_values[0]}>My Gatepass Request</option>
-                <option value={dropdown_values[1]}>Others Gatepass Request</option>
+              { dropdownValues.map((value) => (
+                <option value={value} key={value}>{value}</option>
+              ))}
             </select>
       </div>
     
