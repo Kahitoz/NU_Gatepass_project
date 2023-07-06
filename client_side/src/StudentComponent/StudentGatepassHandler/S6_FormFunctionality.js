c176756aa27bf8f6a8dd44323a100239f0f70448
kahitoz <kshitiz.sinha.025@gmail.com>
2023-07-07 00:16:49 +0530

    Separated the form functionality

---
+++ client_side/src/StudentComponent/StudentGatepassHandler/S6_FormFunctionality.js
@@ -0,0 +1,127 @@
+import { useEffect, useState } from "react";
+import moment from "moment";
+import Cookies from "js-cookie";
+import LFfunctions from "./S1_LocalFixed";
+import { week } from "./S1_ParameterConfig";
+import S6_FormDesigns from "../StudentSkeleton/S6_Form";
+
+const S6_FormFunctionality = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [wselec, setWselect] = useState("");
+  const [wOpen, setWopen] = useState(false);
+
+  const [departureDateVisible, setDepartureDateVisible] = useState(false);
+  const [departureTimeVisible, setDepartureTimeVisible] = useState(false);
+  const [arrivalDateVisible, setArrivalDateVisible] = useState(false);
+  const [arrivalTimeVisible, setArrivalTimeVisible] = useState(false);
+  const [destinationVisible, setDestinationVisible] = useState(false);
+  const [reasonVisible, setReasonVisible] = useState(false);
+  const [wardenVisible, setWardenVisible] = useState(false);
+
+  const [departureTime, setDepartureTime] = useState("");
+  const [arrivalTime, setArrivalTime] = useState("");
+  const [departureDate, setDepartureDate] = useState("");
+  const [arrivalDate, setArrivalDate] = useState("");
+  const [weekLimit, setWeekLimit] = useState(0);
+  const accessToken = Cookies.get("ACCESS_TOKEN");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let config = await week(accessToken);
+      setDepartureTime(
+        moment(config.departureTime, "HH:mm:ss").format("HH:mm")
+      );
+      setArrivalTime(moment(config.arrivalTime, "HH:mm:ss").format("HH:mm"));
+      setWeekLimit(config.weekLimit);
+    };
+    fetchData();
+
+    setDepartureDate(new Date().toLocaleDateString("en-GB"));
+    setArrivalDate(new Date().toLocaleDateString("en-GB"));
+    console.log("date  = ", departureDate);
+  }, []);
+
+  const handleClick = async () => {
+    try {
+      let localFixedUsed = 0;
+      const check = await LFfunctions.checkLocalFixed(
+        accessToken,
+        departureTime,
+        arrivalTime,
+        localFixedUsed,
+        weekLimit
+      );
+
+      if (check === true) {
+        await LFfunctions.applyLocalFixedGatepass(
+          accessToken,
+          departureDate,
+          departureTime,
+          arrivalDate,
+          arrivalTime,
+          weekLimit
+        );
+        alert("You have successfully applied for Local Fixed Gatepass!");
+      }
+    } catch (error) {
+      console.error(error);
+      // Handle the error here
+      alert("An error occurred while applying for Local Fixed Gatepass!");
+    }
+  };
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    setDepartureDateVisible(true);
+    setDepartureTimeVisible(true);
+    setArrivalDateVisible(true);
+    setArrivalTimeVisible(true);
+    setDestinationVisible(false);
+    setReasonVisible(false);
+    setWardenVisible(false);
+
+    if (option === "Local Flexible") {
+      setReasonVisible(true);
+      setWardenVisible(true);
+    }else if (option === "Outstation" || option === "Emergency") {
+      setDestinationVisible(true);
+      setReasonVisible(true);
+      setWardenVisible(true);
+    }
+  };
+
+  const handleWselect = (option) => {
+    setWselect(option);
+    setWopen(false);
+  };
+
+  const handlewDropDown = () => {
+    setWopen(!wOpen);
+  };
+
+  const Altwardens = ["Warden-1", "Warden-2", "Warden-3"];
+
+  return (
+    <S6_FormDesigns
+      selectedOption={selectedOption}
+      departureDateVisible={departureDateVisible}
+      departureTimeVisible={departureTimeVisible}
+      arrivalDateVisible={arrivalDateVisible}
+      arrivalTimeVisible={arrivalTimeVisible}
+      destinationVisible={destinationVisible}
+      reasonVisible={reasonVisible}
+      wardenVisible={wardenVisible}
+      departureTime={departureTime}
+      arrivalTime={arrivalTime}
+      handleOptionSelect={handleOptionSelect} // Ensure this line is correct
+      handleWselect={handleWselect}
+      handlewDropDown={handlewDropDown}
+      wselec={wselec}
+      wOpen={wOpen}
+      Altwardens={Altwardens}
+      handleClick={handleClick}
+    />
+  );
+};
+
+export default S6_FormFunctionality;
