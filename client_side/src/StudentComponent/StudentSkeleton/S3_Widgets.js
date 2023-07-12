6d6843ea79aa321e68a277f29531e046540b51cb
kahitoz <kshitiz.sinha.025@gmail.com>
2023-07-12 16:36:20 +0530

    Latest Gatepass Status widget Now working

--- client_side/src/StudentComponent/StudentSkeleton/S3_Widgets.js
+++ client_side/src/StudentComponent/StudentSkeleton/S3_Widgets.js
@@ -35,56 +35,58 @@ const S3_Widgets = () => {
     };
     statusCheck();
 
-    async function fetchGatepassData() {
-      try {
-        const authorization = userToken;
-        const headers = {
-          Authorization: authorization,
-          "Content-Type": "application/json",
-        };
 
-        const response = await fetch(
-          "http://127.0.0.1:4000/gatepass/v2/student/recent_gatepass",
-          { headers }
-        );
+
+    async function fetchgatepassstatus() {
+      try {
+        const accessToken = Cookies.get("ACCESS_TOKEN");
+
+        const response = await fetch("http://127.0.0.1:4000/gatepass/v2/student/recent_gatepass", {
+          headers: {
+            Authorization: `${accessToken}`,
+          },
+        });
+
         const data = await response.json();
 
-        if (data.length > 0) {
-          const latestGatepass = data[data.length - 1];
-          setGatepassStatus(latestGatepass.status);
+        if (Array.isArray(data) && data.length > 0) {
+          const latestGatepassStatus = data[0].status;
+          setGatepassStatus(latestGatepassStatus);
         }
       } catch (error) {
-        console.error("Error fetching gatepass data:", error);
+        console.error("Error fetching gatepass status:", error);
       }
     }
 
-    fetchGatepassData();
+
+
+    fetchgatepassstatus()
   }, []);
 
   return (
-    <div className={`${designs.d1}`}>
-      <div className={`${designs.d2}`}>
-        <div
-          className={`items-center justify-center text-center flex flex-col sm:flex-row`}
-        >
-          <h1 className={`font-bold me-2`}>Local Fixed Gatepass Available:</h1>
-          <p>{getWeekLimit}</p>
+      <div className={`${designs.d1}`}>
+        <div className={`${designs.d2}`}>
+          <div
+              className={`items-center justify-center text-center flex flex-col sm:flex-row`}
+          >
+            <h1 className={`font-bold me-2`}>Local Fixed Gatepass Available:</h1>
+            <p>{getWeekLimit}</p>
+          </div>
+        </div>
+
+        <div className={`${designs.d2}`}>
+          <h1 className={`text-green-500 font-bold`}>{statusMessage}</h1>
+        </div>
+
+        <div className={`${designs.d2}`}>
+          <div
+              className={`items-center justify-center text-center flex flex-col sm:flex-row`}
+          >
+            <h1 className={`font-bold me-2`}>Last Gatepass Status:</h1>
+            <p className={`text-blue-700`}>{gatepassStatus}</p>
+          </div>
         </div>
       </div>
-
-      <div className={`${designs.d2}`}>
-        <h1 className={`text-green-500 font-bold`}>{statusMessage}</h1>
-      </div>
-
-      <div className={`${designs.d2}`}>
-        <div
-          className={`items-center justify-center text-center flex flex-col sm:flex-row`}
-        >
-          <h1 className={`font-bold me-2`}>Last Gatepass Status:</h1>
-          <p className={`text-blue-700`}>{gatepassStatus}</p>
-        </div>
-      </div>
-    </div>
   );
 };
 
