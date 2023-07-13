14a93e8b487052f1870526425590a21bd5170b4a
Karam <Karam.arora20@st.niituniversity.in>
2023-07-14 01:58:16 +0530

    widget updates

--- client_side/src/ChiefWardenComponent/ChiefWardenSkeleton/CW3_Widgets.js
+++ client_side/src/ChiefWardenComponent/ChiefWardenSkeleton/CW3_Widgets.js
@@ -1,9 +1,12 @@
 import designs from "../ChiefWardenStyling/CW3_WidgetsCSS"
 import { useEffect,useState } from "react";
 import Cookies from "js-cookie";
+import { useLocation } from "react-router-dom";
 const W3_Widgets = ({setGpDropdown,dropdownValues,totalPending}) => {
   const userToken = Cookies.get("ACCESS_TOKEN");
   const [totalStudents, setTotalStudents] = useState(0);
+  const current=useLocation().pathname;
+  // console.log(current);
   useEffect(() => {
     fetch(
    `http://127.0.1:4000/gatepass/v2/admin/student_in_campus`,
@@ -42,13 +45,24 @@ const W3_Widgets = ({setGpDropdown,dropdownValues,totalPending}) => {
       </div>
       </div>
 
-      <div className={`${designs.d2}`}>
+      {current==='/ChiefWarden/home' && <div className={`${designs.d2}`}>
       <div className={`items-center justify-center text-center flex flex-col sm:flex-row`}>
          <h1 className={`font-bold me-2`}>Total Pending Requests:</h1>
          <p >{totalPending}</p>
       </div>
-       
+      </div>}
+      {current==='/ChiefWarden/home/AutoApproved' && <div className={`${designs.d2}`}>
+      <div className={`items-center justify-center text-center flex flex-col sm:flex-row`}>
+         <h1 className={`font-bold me-2`}>Currently AutoApproved Batches:</h1>
+         <p >00</p>
       </div>
+      </div>}
+      {current==='/ChiefWarden/home/Blocked' && <div className={`${designs.d2}`}>
+      <div className={`items-center justify-center text-center flex flex-col sm:flex-row`}>
+         <h1 className={`font-bold me-2`}>Currently Blocked Students:</h1>
+         <p >{totalPending}</p>
+      </div>
+      </div>}
     </div>
   );
 };
