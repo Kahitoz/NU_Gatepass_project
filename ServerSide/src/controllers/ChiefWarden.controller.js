967f215dcf7cdee106c9a03402dd2e7f7c436bbf
Karam <Karam.arora20@st.niituniversity.in>
2023-07-13 14:59:24 +0530

    api bug resolved

--- ServerSide/src/controllers/ChiefWarden.controller.js
+++ ServerSide/src/controllers/ChiefWarden.controller.js
@@ -1,30 +1,30 @@
 import { getConnection, sql, queries } from "../database";
 const date = require("date-and-time");
-export const blacklistedStudentWise=()=>async (req, res) => {
+
+  export const blacklistedStudentWise = async (req, res) => {
     try {
       const pool = await getConnection();
-      const result = await pool.request().query(queries.getBlacklistedStudentWise);
+      const result = await pool
+        .request()
+        .query(queries.getBlacklistedStudentWise);
       return res.json(result.recordset);
     } catch (error) {
-      res.status(500);
-      res.send(error.message);
+      res.status(500).send(error.message);
     }
-  
-  }
-  export const blacklistedGroupWise=()=>async (req, res) => {
+  };
+  export const blacklistedGroupWise=async (req, res) => {
     try {
         const pool = await getConnection();
         const result = await pool
           .request()
           .query(queries.getBlacklistedGroupWise);
-        return res.send(result.recordset);
+        return res.json(result.recordset);
       } catch (error) {
-        res.status(500);
-        res.send(error.message);
+        res.status(500).send(error.message);
       }
 
   }
-  export const AutoApprovedAll=()=>async (req, res) => {
+  export const AutoApprovedAll=async (req, res) => {
     try{
         const pool = await getConnection();
         const result= await pool
