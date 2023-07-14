7c83edee29ab230ccaa00bfc4296bf77abcfdcb4
Karam <Karam.arora20@st.niituniversity.in>
2023-07-14 15:54:35 +0530

    CW Notifications

--- client_side/src/ChiefWardenComponent/ChiefWardenScreens/ChiefWardenNotifications.js
+++ client_side/src/ChiefWardenComponent/ChiefWardenScreens/ChiefWardenNotifications.js
@@ -2,39 +2,39 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../ChiefWardenSkeleton/CW1_Navbar";
 import SubNavbar from "../ChiefWardenSkeleton/CW2_SubNavbar";
 import Widgets from "../ChiefWardenSkeleton/CW3_Widgets"
-import Table from "../ChiefWardenSkeleton/CW4_tableComponents/CW4_1_pendingTable"
+import Table from "../ChiefWardenSkeleton/CW4_tableComponents/CW4_6_NotificationTable"
 import Cookies from "js-cookie";
+import NotificationForm from "../ChiefWardenSkeleton/CW8_NotificationsForm"
 
-const WardenPendingRequest = () => {
+const ChiefWardenNotifications = () => {
   const accessToken = Cookies.get("ACCESS_TOKEN");
-  // console.log(accessToken);
   const tabs = ["Gatepass Requests", "AutoApproved","Blocked", "Notifications", "Profile Requests"]
-  const [GpDropdown, setGpDropdown] = useState("MyGatepassRequest")
-  const dropdownValues=["MyGatepassRequest", "OthersGatepassRequest"]
-  const [Tb_data_Api, setTb_data_Api] = useState("http://localhost:4000/gatepass/v2/warden/get_dashboard_my");
+  const [GpDropdown, setGpDropdown] = useState("Currently Active")
+  const dropdownValues=["Currently Active", "Notifications History"]
+  const [Tb_data_Api, setTb_data_Api] = useState("set api here");
   const [data, setData] = useState([]);
  
-  useEffect( () => {
-    if (GpDropdown === "MyGatepassRequest") {
-      setTb_data_Api("http://localhost:4000/gatepass/v2/warden/get_dashboard_my");
-    } else {
-      setTb_data_Api("http://localhost:4000/gatepass/v2/warden/get_dashboard_others");
-    }
-    const fetchData = async () => {
-      try {
-        const response = await fetch(Tb_data_Api, {
-          headers: {
-            Authorization: accessToken,
-          },
-        });
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, [GpDropdown, Tb_data_Api, accessToken])
+  // useEffect( () => {
+  //   if (GpDropdown === "Currently Active") {
+  //     setTb_data_Api("set api here");
+  //   } else {
+  //     setTb_data_Api("set api here");
+  //   }
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch(Tb_data_Api, {
+  //         headers: {
+  //           Authorization: accessToken,
+  //         },
+  //       });
+  //       const jsonData = await response.json();
+  //       setData(jsonData);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, [GpDropdown, Tb_data_Api, accessToken])
 
   return (
     <div className="w-screen h-screen bg-background">
@@ -47,11 +47,16 @@ const WardenPendingRequest = () => {
       <div>
         <Widgets setGpDropdown={setGpDropdown} dropdownValues={dropdownValues} />
       </div>
-      <div>
+      <div className="bg-background flex justify-between px-4 py-4 flex-col sm:flex-row sm:items-start">
+      <div className="flex-1 ">
         <Table data={data}  />
+        </div>
+        <div className="flex-1">
+        <NotificationForm/>
+      </div>
       </div>
     </div>
   );
 };
 
-export default WardenPendingRequest;
+export default ChiefWardenNotifications;
