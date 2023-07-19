80db1d51e3068fccb97999e3eaf40f6e23d84483
kahitoz <kshitiz.sinha.025@gmail.com>
2023-07-19 18:01:04 +0530

    Removed the useless route

--- ServerSide/src/routes/warden.routes.js
+++ ServerSide/src/routes/warden.routes.js
@@ -6,7 +6,6 @@ import {
 
 const router = Router();
 
-router.get("/warden/gatepass_approve_or_reject/:id/:flag", gatepassApproveOrReject);
 router.put("/warden/approve_gatepass", approveGatepass);
 router.put('/warden/reject_Gatepass', rejectGatepass) // requires gatepass req_id and comments from the client
 router.get('/warden/get_all_gatepass', getAllGatepasses);
