36f16ac188acf182f069f99df7eab28e782c924f
Karam <Karam.arora20@st.niituniversity.in>
2023-07-23 19:14:14 +0530

    insert Tower Warden function

--- ServerSide/src/controllers/ChiefWarden.controller.js
+++ ServerSide/src/controllers/ChiefWarden.controller.js
@@ -98,3 +98,22 @@ const date = require("date-and-time");
         res.send(error.message);
     }
   }
+
+  export const insertTowerWarden=async (req, res) => {
+    try{
+        const pool = await getConnection();
+        const result= await pool
+        .request()
+        .input('hostel_name',sql.VarChar,req.body.hostel_name)
+        .input('hostel_id',sql.Int,req.body.hostel_id)
+        .input('warden_id',sql.VarChar,req.body.warden_id)
+        .input('warden_name',sql.VarChar,req.body.warden_name)
+        .query(queries.insertTowerWarden)
+        return res.send(result.recordset)
+    }
+    catch(error){
+        res.status(500);
+        console.log(error.message);
+        res.send(error.message);
+    }
+  }
