afb148c2a925df0bc9db8ce1982b9c7685d1cdcc
Karam <Karam.arora20@st.niituniversity.in>
2023-07-23 17:32:11 +0530

    getAllWardens route

--- ServerSide/src/routes/chiefWarden.routes.js
+++ ServerSide/src/routes/chiefWarden.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {
-    blacklistedStudentWise,blacklistedGroupWise,AutoApprovedAll,wardenGatepassDetails,getAllGatePassesToday,getHostelTowers
+    blacklistedStudentWise,blacklistedGroupWise,AutoApprovedAll,wardenGatepassDetails,getAllGatePassesToday,getHostelTowers,getAllWardens
 } from "../controllers/ChiefWarden.controller";
 
 const router = Router();
@@ -11,6 +11,7 @@ router.get("/ChiefWarden/blacklistedGroupWise", blacklistedGroupWise);
 router.get("/ChiefWarden/WardenGatepassDetails", wardenGatepassDetails);
 router.get("/ChiefWarden/getAllGatePassesToday", getAllGatePassesToday);
 router.get("/ChiefWarden/getHostelTowers/:hostel", getHostelTowers);
+router.get("/ChiefWarden/getAllWardens", getAllWardens);
 
 
 export default router;
