f868d28381ddb4a7516e81f821f89977ee6292b3
Karam <Karam.arora20@st.niituniversity.in>
2023-07-23 17:32:29 +0530

    getAllWardens functions

--- ServerSide/src/controllers/ChiefWarden.controller.js
+++ ServerSide/src/controllers/ChiefWarden.controller.js
@@ -26,6 +26,17 @@ const date = require("date-and-time");
       res.status(500).send(error.message);
     }
   };
+  export const getAllWardens = async (req, res) => {
+    try {
+      const pool = await getConnection();
+      const result = await pool
+        .request()
+        .query(queries.getAllWardens);
+      return res.json(result.recordset);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  };
 
   export const getAllGatePassesToday = async (req, res) => {
     try {
