cde7ea98485208728da47736ceacfe5403300310
kahitoz <kshitiz.sinha.025@gmail.com>
2023-07-24 12:17:05 +0530

    Users Search Function UI Implemented

--- client_side/src/AdminComponent/A5_Handlers/A5_AllUsersTable.js
+++ client_side/src/AdminComponent/A5_Handlers/A5_AllUsersTable.js
@@ -1,48 +1,52 @@
 import designs from "../A3_AdminStyling/A2_GatepassTableStyling";
 import React from "react";
 
-const A5_AllUsersTable = ({userData, handleNextPage, handlePreviousPage, page_number}) =>{
-    if (!userData || !Array.isArray(userData)) {
-        return <div>Loading...</div>;
-    }
+const A5_AllUsersTable = ({ userData, handleNextPage, handlePreviousPage, page_number, searchQuery, setSearchQuery }) => {
 
-    const startingIndex = (page_number - 1) * 5;
+    const filteredUserData = userData.filter((item) =>
+        item.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
-    return(
+
+    const totalFilteredPages = Math.ceil(filteredUserData.length / 5);
+
+    const filteredStartingIndex = (page_number - 1) * 5;
+
+    const usersToDisplay = filteredUserData.slice(filteredStartingIndex, filteredStartingIndex + 5);
+
+    return (
         <div className={`bg-background`}>
-
             <div>
                 <div className={`${designs.d1}`}>
-                    <div >
+                    <div>
                         <input
                             className={`rounded-xl px-2`}
-
                             placeholder={`Search User`}
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
                         />
                     </div>
                     <div className={`${designs.d2} `}>
                         <div className={`${designs.d5} `}>Sno.</div>
-                        <div className={`${designs.d5} `} >Email Id</div>
-                        <div className={`${designs.d5} `} >Name </div>
-                        <div className={`${designs.d5} `} >Phone </div>
+                        <div className={`${designs.d5} `}>Email Id</div>
+                        <div className={`${designs.d5} `}>Name </div>
+                        <div className={`${designs.d5} `}>Phone </div>
                     </div>
                 </div>
 
                 <div className={`${designs.d3}`}>
-                    {userData.map((item, idx) =>(
+                    {usersToDisplay.map((item, idx) => (
                         <div className={`${designs.d4}`} key={idx}>
-                            <h1 className={`${designs.d5}` }>{idx+startingIndex+1}</h1>
-                            <h1 className={`${designs.d5} overflow-x-scroll` }>{item.email_id}</h1>
-                            <h1 className={`${designs.d5} ` }>{item.name}</h1>
-                            <h1 className={`${designs.d5}` }>{item.contact_number}</h1>
+                            <h1 className={`${designs.d5}`}>{idx + filteredStartingIndex + 1}</h1>
+                            <h1 className={`${designs.d5} overflow-x-scroll`}>{item.email_id}</h1>
+                            <h1 className={`${designs.d5}`}>{item.name}</h1>
+                            <h1 className={`${designs.d5}`}>{item.contact_number}</h1>
                         </div>
-                    ))
-                    }
+                    ))}
                 </div>
             </div>
 
             <div className="flex justify-center mt-4 bg-background">
-
                 <button
                     className="px-4 py-2 mx-2 bg-blue-500 text-white rounded"
                     onClick={handlePreviousPage}
@@ -50,19 +54,17 @@ const A5_AllUsersTable = ({userData, handleNextPage, handlePreviousPage, page_nu
                 >
                     Previous
                 </button>
-                <div className="mx-2 bg-background mt-2 ">Page: {page_number}</div>
+                <div className="mx-2 bg-background mt-2">Page: {page_number} / {totalFilteredPages}</div>
                 <button
                     className="px-4 py-2 mx-2 bg-blue-500 text-white rounded"
                     onClick={handleNextPage}
-                    disabled={userData.length < 5}
+                    disabled={page_number >= totalFilteredPages}
                 >
                     Next
                 </button>
             </div>
-
         </div>
-
-
     );
 };
+
 export default A5_AllUsersTable;
