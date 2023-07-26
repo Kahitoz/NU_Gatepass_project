8ba61a9accef2d01bf640df114842c4b95db8a54
kahitoz <kshitiz.sinha.025@gmail.com>
2023-07-26 14:38:55 +0530

    Para Style Changed

--- client_side/src/GlobalComponent/G1_Modals/G1_MessageModal.js
+++ client_side/src/GlobalComponent/G1_Modals/G1_MessageModal.js
@@ -4,9 +4,9 @@ const G1_MessageModal = ({title, message, action}) =>{
     return(
         <>
             <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur">
-                <div className="bg-background border border-black w-4/12 p-6 rounded-md">
+                <div className="bg-background flex flex-col border border-black w-4/12 p-6 rounded-md items-center justify-center">
                     <h1 className="text-xl font-bold mb-4 text-center">{title}</h1>
-                    <p className={"m-2 p-1 rounded-md w-full text-center"}>{message}</p>
+                    <p className={"m-2  rounded-md w-full text-center "}>{message}</p>
                     <div className="flex justify-center">
                         <button
                             className="m-2 p-2 bg-blue-500 text-white"
